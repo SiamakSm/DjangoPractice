ed2f5b3f6340db09f6658ffa5b300ed1d629ff6b
SiamakSm <siamak036@gmail.com>
2026-09-23 21:34:24 +0200

    refactor: clean up URLs

--- demoApp/urls.py
+++ demoApp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('greet/<str:name>/',views.say_my_name),
     path('age/<int:age>/',views.guess_my_age),
     path('info/<str:name>/<int:age>/',views.guess_my_info),
+    path('info/<str:name>/',views.say_my_name),
 
 ]
 
