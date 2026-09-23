6667e1a6f5db3a87cdf29a3f7f6afc5c2aa117c1
SiamakSm <siamak036@gmail.com>
2026-09-24 01:08:26 +0200

    feat: add Student model

--- demoApp/models.py
+++ demoApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    Name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"His name is {self.Name} and ({self.age}) years old and his email is {self.email}"
+    
